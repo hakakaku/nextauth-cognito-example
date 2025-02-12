dd56c844006e3fb5f079a5cb5806e5e2d053da93
lihaku <lihaku@team-lab.com>
2025-02-12 12:09:04 +0900

    fix: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
-import { getServerSession } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import Link from "next/link";
 
 export default async function Page() {
-  const session = await getServerSession();
-
-  if (!session) {
-    redirect("/login");
-  } else {
-    redirect("/orders");
-  }
+  return (
+    <>
+      <h1 className="text-4xl text-center">
+        Next Auth V4 + AWS Cognito Example
+      </h1>
+      <ul className="flex justify-center space-x-4">
+        <Link
+          href="/login"
+          className="text-blue-500 hover:underline text-lg p-4"
+        >
+          Login Page
+        </Link>
+        <Link
+          href="/protected"
+          className="text-blue-500 hover:underline text-lg p-4"
+        >
+          Middleware-Protected Page
+        </Link>
+        <Link
+          href="/orders"
+          className="text-blue-500 hover:underline text-lg p-4"
+        >
+          Orders Page
+        </Link>
+      </ul>
+    </>
+  );
 }
