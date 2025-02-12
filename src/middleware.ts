1d7f5b95a00f44d7fc2debf5378c34424a6ac990
lihaku <lihaku@team-lab.com>
2025-02-12 15:32:21 +0900

    fix: export default middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,3 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: {
-    signIn: "/login",
-    error: "/login",
-  },
-});
+export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/protected"] };
